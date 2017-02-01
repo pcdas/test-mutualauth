f4caf04fc375c3d7ff4d805ccacbc4580d57e302
pdas <pdas@informatica.com>
2017-02-01 14:33:36 -0800

    Runs the test against an external webserver and verifies the hostname by comparing against the common name from the server's certificate.

--- src/test/java/mutualauth/MyServiceTest3.java
+++ src/test/java/mutualauth/MyServiceTest3.java
@@ -8,53 +8,75 @@ import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.TrustManagerFactory;
 
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ContextConfiguration
 @TestPropertySource("/my-test.properties")
 public class MyServiceTest3 {
 
-    @LocalServerPort
+	private final Log logger = LogFactory.getLog(getClass());
+	@Value("${server.ssl.hostname}")
+	private String hostname;
+	@Value("${server.ssl.port}")
     private int port;
 
     @Value("${client.ssl.key-store}")
     private String keyStore;
     @Value("${client.ssl.key-store-password}")
-    private String keyStorePassword;
+    private char[] keyStorePassword;
+
+    @Value("${client.ssl.trust-store}")
+    private String trustStore;
+    @Value("${client.ssl.trust-store-password}")
+    private char[] trustStorePassword;
+
     private SSLContext ctx;
 
     private URL base;
 
     @Before
     public void setUp() throws Exception {
-        this.base = new URL("https://localhost:" + port + "/index");
+        this.base = new URL("https://" + hostname + ":" + port + "/index");
 
         ctx = SSLContext.getInstance("TLS");
         KeyStore ks = KeyStore.getInstance("PKCS12");
-        ks.load(new FileInputStream(keyStore), keyStorePassword.toCharArray());
+        ks.load(new FileInputStream(keyStore), keyStorePassword);
         KeyManagerFactory kmf = KeyManagerFactory.getInstance(
         		KeyManagerFactory.getDefaultAlgorithm());
-        kmf.init(ks, keyStorePassword.toCharArray());
-        ctx.init(kmf.getKeyManagers(), null, SecureRandom.getInstanceStrong());
+        kmf.init(ks, keyStorePassword);
+
+        KeyStore ts = KeyStore.getInstance("JKS");
+        ts.load(new FileInputStream(trustStore), trustStorePassword);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(
+            TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(ts);
+        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), SecureRandom.getInstanceStrong());
     }
 
 	@Test
@@ -63,7 +85,7 @@ public class MyServiceTest3 {
 			HttpClients
 				.custom()
 				.setSSLContext(ctx)
-				.setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
+				.setSSLHostnameVerifier(this::verifyHost).build();
 		HttpComponentsClientHttpRequestFactory requestFactory =
 				new HttpComponentsClientHttpRequestFactory();
 		requestFactory.setHttpClient(httpClient);
@@ -74,6 +96,30 @@ public class MyServiceTest3 {
 		assertThat(response.getStatusCode().value(), equalTo(200));
 		assertThat(response.getBody(),
 				equalTo("Hello from MyService authenticated with x509 certificate...\n"));
+	}
+
+	private boolean verifyHost(String hostname, javax.net.ssl.SSLSession sess) {
+		try {
+			if (logger.isDebugEnabled()) {
+				Certificate[] certs = sess.getPeerCertificates();
+				String cert = String.format("verifyHost certificate: type=%s [%s]",
+						certs[0].getType(), certs[0]);
+				logger.info(cert);
+				String s = String.format("verifyHost hostname=%s, peerHost=%s, peerPort=%d",
+						hostname, sess.getPeerHost(), sess.getPeerPort());
+				logger.info(s);
+			}
+			return hostname.equalsIgnoreCase(extractCommonName(sess.getPeerPrincipal().toString()));
+		} catch (SSLPeerUnverifiedException e) {
+			logger.info("Hostname verification failed.", e);
+			return false;
+		}
+	}
+
+	private String extractCommonName(String certPrincipal) {
+		logger.info(String.format("verifyHost peerPrincipal=[%s]", certPrincipal));
+		Matcher m = Pattern.compile("CN=(.*?),").matcher(certPrincipal);
+		return m.find() ? m.group(1) : "";
 	}
 
 }
